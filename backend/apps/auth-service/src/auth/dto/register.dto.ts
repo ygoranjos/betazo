75d6abec727972adac63c8f0155667eb7129de67
João M <joaovicentem.dev@gmail.com>
2026-04-28 01:12:51 -0300

    padroniza header do projeto

--- backend/apps/auth-service/src/auth/dto/register.dto.ts
+++ backend/apps/auth-service/src/auth/dto/register.dto.ts
@@ -7,7 +7,7 @@ export class RegisterDto {
   @IsString()
   @MinLength(3)
   @MaxLength(30)
-  @Matches(/^[a-zA-Z0-9_]+$/, { message: 'username deve conter apenas letras, números e _' })
+  @Matches(/^[a-zA-Z0-9_]+$/, { message: 'Username deve conter apenas letras, números e _' })
   username: string;
 
   @IsString()
